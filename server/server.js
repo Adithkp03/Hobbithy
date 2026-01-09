bf4b1d7ce58a4b1ab34fff349de4a7aa0c83e8e6
Adithkp03 <adithkp03@gmail.com>
2026-01-09 11:35:57 +0530

    drafting

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-30:
+
 if (require.main === module) {
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
